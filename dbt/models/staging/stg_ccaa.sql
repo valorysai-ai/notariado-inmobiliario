457e269ae49a0045b810fcd390aaabdd9ee8c6f5
oscar-ctrl <128899588+oscar-ctrl@users.noreply.github.com>
2026-07-05 15:36:36 +0000

    fix: use relative paths in dbt staging models

--- dbt/models/staging/stg_ccaa.sql
+++ dbt/models/staging/stg_ccaa.sql
@@ -1,32 +1,24 @@
 with source as (
     select * from read_parquet(
-        '/workspaces/notariado-inmobiliario/data/raw/ccaa/*/data.parquet'
+        'data/raw/ccaa/*/data.parquet'
     )
 ),
 
 renamed as (
     select
-        -- ids
         objectid,
         cod_ccaa,
         name_ccaa,
         ccaa_id_new,
-
-        -- categorías
         tipo_construccion_id,
         clase_finca_urbana_id,
         es_estimado,
-
-        -- métricas
         precio_m2,
         precio_medio,
         superficie_media,
         total_informados,
         total,
-
-        -- metadata
         snapshot_date::date as snapshot_date
-
     from source
 )
 
--- dbt/models/staging/stg_codigo_postal.sql
+++ dbt/models/staging/stg_codigo_postal.sql
@@ -1,31 +1,23 @@
 with source as (
     select * from read_parquet(
-        '/workspaces/notariado-inmobiliario/data/raw/codigo_postal/*/data.parquet'
+        'data/raw/codigo_postal/*/data.parquet'
     )
 ),
 
 renamed as (
     select
-        -- ids
         objectid,
         cp,
         cp_id_new,
-
-        -- categorías
         tipo_construccion_id,
         clase_finca_urbana_id,
         es_estimado,
-
-        -- métricas
         precio_m2,
         precio_medio,
         superficie_media,
         total_informados,
         total,
-
-        -- metadata
         snapshot_date::date as snapshot_date
-
     from source
 )
 
